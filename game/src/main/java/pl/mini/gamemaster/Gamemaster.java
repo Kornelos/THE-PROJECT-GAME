bcb8a7031a53d6acc6a953aff347b796b3ed4152
rajchm <kapiszon9*>
2020-03-18 16:08:45 +0100

    GameMaster class initial

--- game/src/main/java/pl/mini/gamemaster/Gamemaster.java
+++ game/src/main/java/pl/mini/gamemaster/Gamemaster.java
@@ -1,6 +1,55 @@
 package pl.mini.gamemaster;
 
-public class Gamemaster {
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.UUID;
+import java.lang.Object;
+import java.net.InetAddress;
+
+public class Gamemaster {
     private int portNumber;
+    private InetAddress ipAddress;
+    private final List<UUID> teamRedGuids;
+    private final List<UUID> teamBlueGuids;
+    /*
+    @Getter
+    @Setter
+    private GameMasterBoard board;
+    */
+    @Getter
+    @Setter
+    private GameMasterStatus status;
+
+    //private GameMasterConfiguration configuration;
+
+    public Gamemaster()
+    {
+
+    }
+    /*
+    public GameMasterConfiguration loadConfigurationFromJson(String path)
+    {
+
+    }
+    */
+    public void saveConfigurationToJson(String path)
+    {
+
+    }
+
+    private void putNewPiece()
+    {
+
+    }
+
+    private void printBoard()
+    {
+
+    }
+
+    public void messageHandler(String message)
+    {
+
+    }
 }
