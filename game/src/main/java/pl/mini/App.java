c2d437cdfa6b03465a150b127ff1b39d75ac2bf1
rajchm <kapiszon9*>
2020-03-23 21:55:19 +0100

    App.java - added

---
+++ game/src/main/java/pl/mini/App.java
@@ -0,0 +1,24 @@
+package java.pl.mini;
+
+
+/**
+
+ * Hello world!
+
+ *
+
+ */
+
+public class App
+
+{
+
+    public static void main( String[] args )
+
+    {
+
+        System.out.println( "Hello World!" );
+
+    }
+
+}
