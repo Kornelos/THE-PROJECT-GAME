0ea3bca9c838cb4efdb3db42219f176bd7e067cc
Julian Szachowicz <julzerinos@gmail.com>
2020-03-23 21:21:02 +0100

    Fix AppTest conflict

--- game/src/test/pl/mini/AppTest.java
+++ game/src/test/pl/mini/AppTest.java
@@ -1,5 +1,12 @@
 package pl.mini;
 
-public class AppTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
 }
