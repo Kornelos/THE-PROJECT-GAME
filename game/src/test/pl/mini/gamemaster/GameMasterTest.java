42efd179fcb1317e9e8c11294abff525d1359946
tluczekk <tluczekk@o2.pl>
2020-03-29 21:28:53 +0200

    Merged save and load json tests into one

--- game/src/test/pl/mini/gamemaster/GameMasterTest.java
+++ game/src/test/pl/mini/gamemaster/GameMasterTest.java
@@ -13,38 +13,26 @@ import java.nio.file.Paths;
 
 public class GameMasterTest
 {
-    // TODO:
-    // - Find a way to compare two JSONs
-    // - Resolve problems with shamProbability values inside JSONs
-    @Test
-    public void testLoadFromJSON(){
-        GameMaster gm = new GameMaster();
-        GameMasterConfiguration gc1 = new GameMasterConfiguration(0.8,6,7, new Point[]{new Point(3, 4), new Point(5, 6), new Point(7, 8)},8,
-                                                              6, 2, 2, 1, 1, 2, 3, 5, 2);
-        // gm.setConfiguration(gm.loadConfigurationFromJson("./game/src/test/pl/mini/TestJSONs/test1.json"));
-        //Assert.assertEquals(gm.getConfiguration(), gc1);
-        GameMaster gm2 = new GameMaster();
-        GameMasterConfiguration gc2 = new GameMasterConfiguration(0.4,4,8, new Point[]{new Point(1, 2), new Point(3, 4), new Point(5, 6)},10,
-                                                                  7, 3, 3, 2, 2, 3, 4, 2, 3);
-        //gm2.setConfiguration(gm2.loadConfigurationFromJson("./game/src/test/pl/mini/TestJSONs/test2.json"));
-        //Assert.assertEquals(gm2.getConfiguration(), gc2);
-    }
 
     @Test
-    public void testSaveToJSON() throws IOException {
+    public void testLoadAndSaveToJSON() throws IOException {
         GameMaster gm = new GameMaster();
         GameMasterConfiguration gc1 = new GameMasterConfiguration(0.8,6,7, new Point[]{new Point(3, 4), new Point(5, 6), new Point(7, 8)},8,
                                                                   6, 2, 2, 1, 1, 2, 3, 5, 2);
-        ObjectMapper mapper = new ObjectMapper();
+
         gm.setConfiguration(gc1);
         gm.saveConfigurationToJson("./game/src/test/pl/mini/TestJSONs/test1b.json");
-        // Assert.assertEquals(mapper.readTree((Files.readAllLines(Paths.get("./game/src/test/pl/mini/TestJSONs/test1.json"))).toString()),mapper.readTree((Files.readAllLines(Paths.get("./game/src/test/pl/mini/TestJSONs/test1b.json"))).toString()) );
-        // Assert.assertSame(Files.readAllBytes(Paths.get("./game/src/test/pl/mini/TestJSONs/test1.json")), Files.readAllBytes(Paths.get("./game/src/test/pl/mini/TestJSONs/test1b.json")));
         GameMaster gm2 = new GameMaster();
-        GameMasterConfiguration gc2 = new GameMasterConfiguration(0.4,4,8, new Point[]{new Point(1, 2), new Point(3, 4), new Point(5, 6)},10,
+        gm2.setConfiguration(gm2.loadConfigurationFromJson("./game/src/test/pl/mini/TestJSONs/test1b.json"));
+        Assert.assertEquals(gm.getConfiguration(),gm2.getConfiguration());
+        GameMaster gm3 = new GameMaster();
+        GameMasterConfiguration gc3 = new GameMasterConfiguration(0.4,4,8, new Point[]{new Point(1, 2), new Point(3, 4), new Point(5, 6)},10,
                                                                   7, 3, 3, 2, 2, 3, 4, 2, 3);
-        gm2.setConfiguration(gc2);
-        gm2.saveConfigurationToJson("./game/src/test/pl/mini/TestJSONs/test2b.json");
-        //Assert.assertSame(Files.readAllBytes(Paths.get("./game/src/test/pl/mini/TestJSONs/test2.json")), Files.readAllBytes(Paths.get("./game/src/test/pl/mini/TestJSONs/test2b.json")));
+        gm3.setConfiguration(gc3);
+        gm3.saveConfigurationToJson("./game/src/test/pl/mini/TestJSONs/test2b.json");
+        GameMaster gm4 = new GameMaster();
+        gm4.setConfiguration(gm4.loadConfigurationFromJson("./game/src/test/pl/mini/TestJSONs/test2b.json"));
+        Assert.assertEquals(gm3.getConfiguration(),gm4.getConfiguration());
+
     }
 }
