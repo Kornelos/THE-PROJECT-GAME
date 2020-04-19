b4f12db6b48fa82377790bf8cdcfc3726b03f2ed
Kornelos <kornel1902@gmail.com>
2020-04-19 13:27:54 +0200

    Server creates connection group and is able to communicate with all connected instances.

--- game/src/main/pl/mini/communication/SimpleTCPChannelHandler.java
+++ game/src/main/pl/mini/communication/SimpleTCPChannelHandler.java
@@ -1,25 +1,32 @@
 package pl.mini.communication;
 
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@ChannelHandler.Sharable
 public class SimpleTCPChannelHandler extends SimpleChannelInboundHandler<String> {
+    // this holds reference to all active channels, also game master connection should be somehow separated.
+    ChannelGroup channels =
+            new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
+        channels.add(ctx.channel());
         log.info(ctx.channel().remoteAddress() + " Channel Active");
+
     }
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String s) {
         log.info(ctx.channel().remoteAddress() + s);
-        ctx.channel().writeAndFlush("bruh moment\n");
+        channels.writeAndFlush("Some bruh just send a message\n");
+
     }
 
-    @Override
-    public void channelInactive(ChannelHandlerContext ctx) {
-        log.info(ctx.channel().remoteAddress() + " Channel Inactive");
-    }
 }
--- game/src/main/pl/mini/communication/SimpleTCPChannelInitializer.java
+++ game/src/main/pl/mini/communication/SimpleTCPChannelInitializer.java
@@ -2,15 +2,26 @@ package pl.mini.communication;
 
 
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
 public class SimpleTCPChannelInitializer extends ChannelInitializer<SocketChannel> {
+    // static resources - they are shared between all connections
+    private static final StringDecoder DECODER = new StringDecoder();
+    private static final StringEncoder ENCODER = new StringEncoder();
+
+    private static final SimpleTCPChannelHandler SERVER_HANDLER = new SimpleTCPChannelHandler();
+
     @Override
     protected void initChannel(SocketChannel socketChannel) {
-        socketChannel.pipeline().addLast(new StringEncoder());
-        socketChannel.pipeline().addLast(new StringDecoder());
-        socketChannel.pipeline().addLast(new SimpleTCPChannelHandler());
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        // the encoder and decoder are static as these are sharable
+        pipeline.addLast(DECODER);
+        pipeline.addLast(ENCODER);
+
+        // and then business logic.
+        pipeline.addLast(SERVER_HANDLER);
     }
 }
