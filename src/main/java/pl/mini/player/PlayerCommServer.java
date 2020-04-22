a492e0b09ad6de7db210f9d9cdb0736fc2115f10
tluczekk <tluczekk@o2.pl>
2020-04-22 14:10:33 +0200

    Added simple integration test using carrotsearch

--- src/main/java/pl/mini/player/PlayerCommServer.java
+++ src/main/java/pl/mini/player/PlayerCommServer.java
@@ -5,6 +5,7 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
@@ -54,5 +55,16 @@ public class PlayerCommServer {
 
     public void closeConnection() {
         group.shutdownGracefully();
+    }
+
+    /**
+     *  Methods used in integration testing
+     */
+    public void evaluate (String msg){
+        channel.writeAndFlush(new TextWebSocketFrame(msg));
+    }
+
+    public void openConnection() throws InterruptedException {
+        this.connect();
     }
 }
