de879498e2cdb8bf600be6cc4ea623a37472a609
Kacper Reja <61394825+nischuGit@users.noreply.github.com>
2020-05-05 12:16:10 +0200

    discover final

--- src/main/java/pl/mini/cell/Cell.java
+++ src/main/java/pl/mini/cell/Cell.java
@@ -3,6 +3,10 @@ package pl.mini.cell;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.json.simple.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Cell {
     @Getter
@@ -16,9 +20,18 @@ public class Cell {
         this.playerGuids = "";
     }
 
+    public String toJsonString() {
+        Map<String, String> jsonMap = new HashMap<>();
+        jsonMap.put("cellState", cellState.name());
+        jsonMap.put("distance", String.valueOf(distance));
+        jsonMap.put("playerGuid", playerGuids);
+
+        JSONObject json = new JSONObject(jsonMap);
+        return json.toString();
+    }
+
     @Override
-    public String toString()
-    {
-        return String.format("cellState: " + cellState + "distance: " + distance + "playerGuids: " + playerGuids);
+    public String toString() {
+        return toJsonString();
     }
 }
