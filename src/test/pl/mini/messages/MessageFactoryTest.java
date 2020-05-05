aa26183796cdae851240703a493950dc9168a0bc
Kornelos <kornel1902@gmail.com>
2020-05-05 16:31:25 +0200

    added test and fixed bug

--- src/test/pl/mini/messages/MessageFactoryTest.java
+++ src/test/pl/mini/messages/MessageFactoryTest.java
@@ -17,54 +17,71 @@ import java.util.UUID;
 public class MessageFactoryTest {
 
     @Test
-    public void testFactory() throws Exception {
+    public void testPlayerConnect() throws Exception {
         ConnectMessage connectMessage = new ConnectMessage(UUID.randomUUID());
 
         // test player connect message creation
         JsonMessage jsonMessagePlayer = MessageFactory.messageFromString(connectMessage.toString());
-        assert jsonMessagePlayer != null;
         Assert.assertEquals(connectMessage.getClass(), jsonMessagePlayer.getClass());
         ConnectMessage cm = (ConnectMessage) jsonMessagePlayer;
         Assert.assertEquals(connectMessage.getPlayerGuid(), cm.getPlayerGuid());
+    }
 
+    @Test
+    public void testGmConnect() throws Exception {
         // test gm connect
         GmConnectMessage gmConnectMessage = new GmConnectMessage();
         JsonMessage jsonMessageGm = MessageFactory.messageFromString(gmConnectMessage.toString());
-        assert jsonMessageGm != null;
         Assert.assertEquals(gmConnectMessage.getClass(), jsonMessageGm.getClass());
+    }
 
+    @Test
+    public void testPlaceMessage() throws Exception {
         //test place message creation
-        PlaceMessage placeMessage= new PlaceMessage(UUID.randomUUID());
+        PlaceMessage placeMessage = new PlaceMessage(UUID.randomUUID());
         JsonMessage jsonMessagePlace = MessageFactory.messageFromString(placeMessage.toString());
-        assert jsonMessagePlace != null;
         Assert.assertEquals(placeMessage.getClass(), jsonMessagePlace.getClass());
         PlaceMessage pm = (PlaceMessage) jsonMessagePlace;
         Assert.assertEquals(placeMessage.getPlayerGuid(), pm.getPlayerGuid());
 
+        // test placement result
+        PlaceResultMessage prm = new PlaceResultMessage(UUID.randomUUID(), PlacementResult.Correct, Status.OK);
+
+        JsonMessage jsonMessagePlacementResult = MessageFactory.messageFromString(prm.toString());
+        Assert.assertEquals(prm.getClass(), jsonMessagePlacementResult.getClass());
+        PlaceResultMessage prmFromFactory = (PlaceResultMessage) jsonMessagePlacementResult;
+        Assert.assertEquals(prmFromFactory.toString(), prm.toString());
+    }
+
+    @Test
+    public void testStartMessage() throws Exception {
         //test start message
         List<UUID> lst = new ArrayList<>();
         lst.add(UUID.randomUUID());
         lst.add(UUID.randomUUID());
         StartMessage startMessage = new StartMessage(TeamColor.Red, TeamRole.Member,
-                3, lst, new Position(6,9), new Board(9,8,7));
+                3, lst, new Position(6, 9), new Board(9, 8, 7));
         JsonMessage jsonMessageStart = MessageFactory.messageFromString(startMessage.toString());
-        assert jsonMessageStart != null;
         Assert.assertEquals(startMessage.getClass(), jsonMessageStart.getClass());
         StartMessage sm = (StartMessage) jsonMessageStart;
         Assert.assertEquals(startMessage, sm);
+    }
 
+    @Test
+    public void testEndMessage() throws Exception {
         //test end message
         EndMessage endMessage = new EndMessage("RED");
         JsonMessage jsonMessageEnd = MessageFactory.messageFromString(endMessage.toString());
-        assert jsonMessageEnd != null;
         Assert.assertEquals(endMessage.getClass(), jsonMessageEnd.getClass());
         EndMessage em = (EndMessage) jsonMessageEnd;
         Assert.assertEquals(endMessage, em);
+    }
 
+    @Test
+    public void testPickupMessage() throws Exception {
         //test pickup message
         PickupMessage pickupMessage = new PickupMessage(UUID.randomUUID());
         JsonMessage jsonMessagePickup = MessageFactory.messageFromString(pickupMessage.toString());
-        assert jsonMessagePickup != null;
         Assert.assertEquals(pickupMessage.getClass(), jsonMessagePickup.getClass());
         PickupMessage pm1 = (PickupMessage) jsonMessagePickup;
         Assert.assertEquals(pickupMessage, pm1);
@@ -72,20 +89,21 @@ public class MessageFactoryTest {
         //test pickup result message
         PickupResultMessage pickupResultMessage = new PickupResultMessage(UUID.randomUUID(), "DENIED");
         JsonMessage jsonMessagePickupResult = MessageFactory.messageFromString(pickupResultMessage.toString());
-        assert jsonMessagePickupResult != null;
         Assert.assertEquals(pickupResultMessage.getClass(), jsonMessagePickupResult.getClass());
         PickupResultMessage prm = (PickupResultMessage) jsonMessagePickupResult;
         Assert.assertEquals(pickupResultMessage, prm);
+    }
 
+    @Test
+    public void testDiscoverMessage() throws Exception {
         //test discover result message
         List<Field> fields = new ArrayList<>();
-        fields.add(new Field(new Position(3,4), new Cell(CellState.Goal)));
-        fields.add(new Field(new Position(5,6), new Cell(CellState.Piece)));
-        fields.add(new Field(new Position(7,8), new Cell(CellState.Empty)));
+        fields.add(new Field(new Position(3, 4), new Cell(CellState.Goal)));
+        fields.add(new Field(new Position(5, 6), new Cell(CellState.Piece)));
+        fields.add(new Field(new Position(7, 8), new Cell(CellState.Empty)));
         DiscoverResultMessage discoverResultMessage = new DiscoverResultMessage(UUID.randomUUID(),
-                new Position(1,2), fields);
+                new Position(1, 2), fields);
         JsonMessage jsonMessageDiscoverResult = MessageFactory.messageFromString(discoverResultMessage.toString());
-        assert jsonMessageDiscoverResult != null;
         Assert.assertEquals(discoverResultMessage.getClass(), jsonMessageDiscoverResult.getClass());
         DiscoverResultMessage drm = (DiscoverResultMessage) jsonMessageDiscoverResult;
         Assert.assertEquals(discoverResultMessage, drm);
