9ac5f2738620a9d7bde12bb013b95a3f8d0cf773
tluczekk <tluczekk@o2.pl>
2020-05-11 16:19:02 +0200

    Integration tests threads implemented

--- src/test/pl/mini/server/GameMasterClientIntegrateTest.java
+++ src/test/pl/mini/server/GameMasterClientIntegrateTest.java
@@ -12,20 +12,61 @@ import pl.mini.player.*;
 
 public class GameMasterClientIntegrateTest{
 
-    @Test
-    public void GMCSequential() {
+    private static Thread trd;
+
+    /**
+     * Sets up the thread for integration test to be executed on
+     * @throws InterruptedException
+     */
+    public void setUp() throws InterruptedException{
+        trd = new Thread(()->{
+            ServerRunner.main(new String[]{"server"});
+            try {
+                this.GMCSequential();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        trd.start();
+        // Giving thread some time to start
+        Thread.sleep(10000);
+    }
+
+    /**
+     * Calls tryPlayer and catches Interrupted Exception
+     */
+    public void GMCSequential() throws InterruptedException {
         try {
             tryGMC();
         }catch(InterruptedException e){
             e.printStackTrace();
+        }finally {
+            this.kill();
         }
     }
 
+    /**
+     * Estabilishes a connection and parses stream for server to evaluate
+     * @throws InterruptedException
+     */
     public void tryGMC() throws InterruptedException {
         GameMasterClient gmc = new GameMasterClient();
         gmc.openConnection(2137);
         String examp = IntStream.range(0, 1024).mapToObj(String::valueOf).collect(Collectors.joining());
         gmc.evaluate(UUID.randomUUID().toString() + ":" + examp);
         gmc.closeConnection();
+    }
+
+    /**
+     * Kills thread and gives it some time to do so
+     * @throws InterruptedException
+     */
+    public void kill() throws InterruptedException {
+        Thread.sleep(1000);
+        trd.interrupt();
+        // waiting for thread to interrupt
+        while(trd.isAlive()){
+            Thread.sleep(200);
+        }
     }
 }
--- src/test/pl/mini/server/PlayerCommServerIntegrateTest.java
+++ src/test/pl/mini/server/PlayerCommServerIntegrateTest.java
@@ -11,21 +11,63 @@ import pl.mini.player.*;
 
 public class PlayerCommServerIntegrateTest{
 
-    @Test
-    public void playerSequential() {
+
+    private static Thread trd;
+
+    /**
+     * Sets up the thread for integration test to be executed on
+     * @throws InterruptedException
+     */
+    public void setUp() throws InterruptedException{
+        trd = new Thread(()->{
+            ServerRunner.main(new String[]{"server"});
+            try {
+                this.playerSequential();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        trd.start();
+        // Giving thread some time to start
+        Thread.sleep(10000);
+    }
+
+    /**
+     * Calls tryPlayer and catches Interrupted Exception
+     */
+    public void playerSequential() throws InterruptedException {
         try {
             tryPlayer();
         }catch(InterruptedException e){
             e.printStackTrace();
+        }finally {
+            this.kill();
         }
     }
 
+    /**
+     * Estabilishes a connection and parses stream for server to evaluate
+     * @throws InterruptedException
+     */
     public void tryPlayer() throws InterruptedException {
-        PlayerCommServer serv = new PlayerCommServer();
-        serv.openConnection();
+        PlayerCommServer playerServer = new PlayerCommServer();
+        playerServer.openConnection();
         String examp = IntStream.range(0, 1024).mapToObj(String::valueOf).collect(Collectors.joining());
-        serv.evaluate(UUID.randomUUID().toString() + ":" + examp);
-        serv.closeConnection();
+        playerServer.evaluate(UUID.randomUUID().toString() + ":" + examp);
+        playerServer.closeConnection();
+    }
+
+    /**
+     * Kills thread and gives it some time to do so
+     * @throws InterruptedException
+     */
+    public void kill() throws InterruptedException {
+        Thread.sleep(1000);
+        trd.interrupt();
+        // waiting for thread to interrupt
+        while(trd.isAlive()){
+            Thread.sleep(200);
+        }
     }
 }
 
